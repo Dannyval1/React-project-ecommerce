b64cd30b263bdeb25faed4be2e14129679ce70ea
devalencia12 <devalencia@bancodeoccidente.com.co>
2022-06-09 14:26:56 -0500

    :sparkles: desafio7-cart-context: Desafio de Contexto para el componente Cart

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,20 +1,20 @@
-import NavBar from './../components/NavBar';
-import ItemListContainer from './../components/ItemListContainer';
-import ItemDetailContainer from './../components/ItemDetailContainer';
+import NavBar from "./../components/NavBar";
+import ItemListContainer from "./../components/ItemListContainer";
+import ItemDetailContainer from "./../components/ItemDetailContainer";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Cart from '../components/Cart';
+import Cart from "../components/Cart";
 
 export default function RoutesApp() {
   return (
-    <BrowserRouter>
-    <NavBar />
-      <Routes>
-        <Route path="/" element={<ItemListContainer />} />
-        <Route path="/category/:id" element={<ItemListContainer />} />
-        <Route path="/item/:id" element={<ItemDetailContainer />} />
-        <Route path="/cart" element={<Cart />} />
-      </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<ItemListContainer />} />
+          <Route path="/category/:id" element={<ItemListContainer />} />
+          <Route path="/item/:id" element={<ItemDetailContainer />} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+      </BrowserRouter>
   );
 }
